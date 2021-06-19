47bed1480cacc61838164e6758669f08645d4715
Rizqi Imam Gilang <widiantorizqi@gmail.com>
2021-06-19 14:10:33 +0700

    first commit

---
+++ PersonalBudget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalBudget
+//
+//  Created by Rizqi Imam Gilang Widianto on 19/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
